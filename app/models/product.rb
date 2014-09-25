2538d4e4d97836610e7dcd59ba11800fd219c369
jahanzaibseyal <seyaljahanzaib@gmail.com>
2014-09-25 17:43:35 +0500

    third commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,11 @@ class Product < ActiveRecord::Base
   scope :extra_quantity,->{where('quantity>=50')}
   # scope :sale, -> { joins(:order_details).where("order_detail.quantity <= ?", 7.days.ago).group("users.id")}
 
+
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates :avatar, :attachment_presence => true
+  validates_with AttachmentPresenceValidator, :attributes => :avatar
+  validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
 end
